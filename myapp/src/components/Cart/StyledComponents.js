7813d3a393a84b78a0912fcec68c2dcca975f209
CHELLAMUTHUMARI P <chellamuthup16072002@gmail.com>
2025-06-27 15:49:34 +0530

    Update StyledComponents.js

--- myapp/src/components/Cart/StyledComponents.js
+++ myapp/src/components/Cart/StyledComponents.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const CartContainer = styled.div`
     background-color: ${props => (props.color)};
-    margin-top: 10vh;
     padding: 15px;
     min-height: 100vh;
     position:relative;
